dd0498af8bec92b2a6d330881340ed403fcdf599
jackorofrobert <dinhson2109@gmail.com>
2023-04-17 22:49:04 +0700

    a

---
+++ js/data.js
@@ -0,0 +1,271 @@
+let products = [
+  {
+    id: 1,
+    title: "Whiskas",
+    price: 10,
+    category: "catfood",
+    image: "/img/Cat/Food/catfood.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 2,
+    title: "Royal Canin",
+    price: 10,
+    category: "catfood 2",
+    image: "/img/Cat/Food/catfood2.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 3,
+    title: "Me-O",
+    price: 10,
+    category: "catfood 3",
+    image: "/img/Cat/Food/catfood3.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 4,
+    title: "Catsrang",
+    price: 10,
+    category: "catfood 4",
+    image: "/img/Cat/Food/catfood4.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 5,
+    title: "Meow Mix",
+    price: 10,
+    category: "catfood 5",
+    image: "/img/Cat/Food/catfood5.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 6,
+    title: "Cat house type 1",
+    price: 100,
+    category: "cathouse",
+    image: "/img/Cat/House/cathouse.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 7,
+    title: "Cat house type 2",
+    price: 100,
+    category: "cathouse 2",
+    image: "/img/Cat/House/cathouse2.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 8,
+    title: "Cat house type 3",
+    price: 100,
+    category: "cathouse 3",
+    image: "/img/Cat/House/cathouse3.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 9,
+    title: "Cat house type 4",
+    price: 100,
+    category: "cathouse 4",
+    image: "/img/Cat/House/cathouse4.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 10,
+    title: "Cat house type 5",
+    price: 100,
+    category: "cathouse 5",
+    image: "/img/Cat/House/cathouse5.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 11,
+    title: "Cat toy type 1",
+    price: 5,
+    category: "cattoy 1",
+    image: "/img/Cat/Toy/cattoy.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 12,
+    title: "Cat toy type 2",
+    price: 5,
+    category: "cattoy 2",
+    image: "/img/Cat/Toy/cattoy2.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 13,
+    title: "Cat toy type 3",
+    price: 5,
+    category: "cattoy 3",
+    image: "/img/Cat/Toy/cattoy3.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 14,
+    title: "Cat toy type 4",
+    price: 5,
+    category: "cattoy 4",
+    image: "/img/Cat/Toy/cattoy4.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 15,
+    title: "Cat toy type 5",
+    price: 5,
+    category: "cattoy 5",
+    image: "/img/Cat/Toy/cattoy5.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 16,
+    title: "Whiskas",
+    price: 10,
+    category: "dogfood",
+    image: "/img/Dog/Food/dogfood.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 17,
+    title: "Royal Canin",
+    price: 10,
+    category: "dogfood 2",
+    image: "/img/Dog/Food/dogfood2.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 18,
+    title: "Me-O",
+    price: 10,
+    category: "dogfood 3",
+    image: "/img/Dog/Food/dogfood3.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 19,
+    title: "Catsrang",
+    price: 10,
+    category: "dogfood 4",
+    image: "/img/Dog/Food/dogfood4.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 20,
+    title: "Meow Mix",
+    price: 10,
+    category: "dogfood 5",
+    image: "/img/Dog/Food/dogfood5.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 21,
+    title: "Dog house type 1",
+    price: 100,
+    category: "doghouse",
+    image: "/img/Dog/House/doghouse.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 22,
+    title: "Dog house type 2",
+    price: 100,
+    category: "doghouse 2",
+    image: "/img/dog/House/doghouse2.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 23,
+    title: "Dog house type 3",
+    price: 100,
+    category: "doghouse 3",
+    image: "/img/Dog/House/doghouse3.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 24,
+    title: "Dog house type 4",
+    price: 100,
+    category: "doghouse 4",
+    image: "/img/Dog/House/doghouse4.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 25,
+    title: "Dog house type 5",
+    price: 100,
+    category: "doghouse 5",
+    image: "/img/Dog/House/doghosue.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 26,
+    title: "Dog toy type 1",
+    price: 5,
+    category: "dogtoy 1",
+    image: "/img/Dog/Toy/dogtoy.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 27,
+    title: "Dog toy type 2",
+    price: 5,
+    category: "dogtoy 2",
+    image: "/img/Dog/Toy/dogtoy2.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 28,
+    title: "Dog toy type 3",
+    price: 5,
+    category: "dogtoy 3",
+    image: "/img/Dog/Toy/dogtoy3.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 29,
+    title: "Dog toy type 4",
+    price: 5,
+    category: "dogtoy 4",
+    image: "/img/Dog/Toy/dogtoy4.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 30,
+    title: "Dog toy type 5",
+    price: 5,
+    category: "dogtoy 5",
+    image: "/img/Dog/Toy/dogtoy5.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+]
